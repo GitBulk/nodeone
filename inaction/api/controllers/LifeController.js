218b8fc4a05c7728e5997045aad068756362f149
gitbulk <giacatiensinh@hotmail.com>
2017-11-24 17:28:58 +0700

    [f] sample controller

---
+++ inaction/api/controllers/LifeController.js
@@ -0,0 +1,11 @@
+var Meaning = require('the-ultimate-question');
+
+module.exports = {
+    purpose: function (req, res) {
+        return  res.json({
+            answer: Meaning.answer(),
+            question: Meaning.question()
+        });
+    }
+};
+
