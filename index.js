f6df74f41719d374f916e2e678138c741ea0b918
gitbulk <giacatiensinh@hotmail.com>
2023-04-07 21:13:40 +0700

    [r] p18 json data

--- index.js
+++ index.js
@@ -4,7 +4,11 @@
 // console.log('from index.js')
 
 
-const math = require('./p15_module_exports')
-const { add, subtract } = math
-console.log(add(3, 4))
-console.log(subtract(2, 8))
+// const math = require('./p15_module_exports')
+// const { add, subtract } = math
+// console.log(add(3, 4))
+// console.log(subtract(2, 8))
+
+const data = require('./p18_data.json')
+
+console.log(data.address.city)
