393f89dd6c37c5453360703f4c1109f06d947114
gitbulk <giacatiensinh@hotmail.com>
2023-04-07 20:43:45 +0700

    [r] p15 import, export module

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const add = require('./add')
-const sum = add(3, 2)
-console.log('sum', sum)
-console.log('from index.js')
+// const add = require('./add')
+// const sum = add(3, 2)
+// console.log('sum', sum)
+// console.log('from index.js')
+
+
+const math = require('./p15_module_exports')
+const { add, subtract } = math
+console.log(add(3, 4))
+console.log(subtract(2, 8))
---
+++ p15_module_exports.js
@@ -0,0 +1,8 @@
+// this is math module
+
+const add = (a, b) => a + b
+const subtract = (a, b) => a - b
+
+module.exports = {
+  add, subtract
+}
