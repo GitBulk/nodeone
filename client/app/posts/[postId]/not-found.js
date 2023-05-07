99b69bb6c2d512f1f50a684c74f1e8f6a7327459
gitbulk <giacatiensinh@hotmail.com>
2023-05-08 01:35:31 +0700

    [r] add Post detail page

---
+++ client/app/posts/[postId]/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className='px-6 prose prose-xl prose-slate dark:prose-invert mx-auto'>
+      <h4 className='text-center'>The requested post does not exist.</h4>
+    </main>
+  )
+}
