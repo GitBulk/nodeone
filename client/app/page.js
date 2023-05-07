8deed4181f7db260f344887c61cce36e9ae23754
gitbulk <giacatiensinh@hotmail.com>
2023-05-08 00:53:44 +0700

    [r] add Navbar

--- client/app/page.js
+++ client/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='px-6 mx-auto'>
       <p className='mt-12 mb-12 text-3xl text-center dark:text-white'>
-        Hello and welcome
+        Hello and welcome 👋 &nbsp;
         <span className='whitespace-nowrap'>
           I am <span className='font-bold'>Kaka</span>
         </span>
