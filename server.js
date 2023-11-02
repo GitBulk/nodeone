bbd2ac24aa356ba1b32726abec7299ab401e6552
GitBulk <giacatiensinh@hotmail.com>
2023-11-02 21:20:32 +0700

    [r] install chalk to print console log

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import app from './src/app.js'
-
+import connect from './src/database/database.js'
 const port = process.env.PORT || 3002
 app.listen(port, async (req, res) => {
+  connect()
   console.log(`Listening on port ${port}`)
 })
--- src/database/database.js
+++ src/database/database.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose'
+import { print, OutputType } from '../helpers/print.js'
 
 async function connect() {
   try {
-    let conenction = await mongoose.connect(process.env.DB_CONNECTION_STRING)
-    console.log('connect mongodb ok')
+    let connection = await mongoose.connect(process.env.DB_CONNECTION_STRING)
+    print('connect mongodb ok', OutputType.SUCCESS)
+    return connection
   } catch (error) {
     throw new Error(error.message)
   }
