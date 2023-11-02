fedce0c4f9faf55e10360bb2708a5c5f2652b4a5
GitBulk <giacatiensinh@hotmail.com>
2023-11-02 15:44:51 +0700

    [r] add express-validator to validate json body request

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,13 +1,23 @@
 import express from 'express'
+import { body, validationResult } from 'express-validator'
+
 const router = express.Router()
 router.get('/', (req, res) => {
   res.send('GET users')
 })
 router.get('/:id', (req, res) => {
-  res.send('GET details users')
+  res.send(`GET details user ${req.params.id}`)
 })
-router.post('/login', (req, res) => {
-  res.send('POST get users')
+router.post('/login',
+  body('email').isEmail(),
+  body('password').isLength({ min: 5 }),
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { email, password } = req.body
+    res.send('POST get users')
 })
 router.post('/register', (req, res) => {
   res.send('POST register users')
